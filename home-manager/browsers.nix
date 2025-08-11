7c132df6dc256f1fe7ce66373bf2fcbba932ccf0
Redwanul Abedin <chromeplated@protonmail.com>
2025-08-11 07:01:56 -0400

    fixed chromium extensions config

--- home-manager/browsers.nix
+++ home-manager/browsers.nix
@@ -28,23 +28,46 @@
       commandLineArgs = [
         "--extension-mime-request-handling=always-prompt-for-install"
       ];
-      extensions = [
-        {
-          # Chromium web store
-          id = "ocaahdebbfolfmndjeplogmgcagdmblk";
-          updateUrl = "https://raw.githubusercontent.com/NeverDecaf/chromium-web-store/master/updates.xml";
-        }
-        {
-          # uBlock origin
-          id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
-          # updateUrl = "";
-        }
-        {
-          # Proton pass
-          id = "ghmbeldphafepmbegfdlkpapadhbakde";
-          # updateUrl = "";
-        }
-      ];
+      extensions =
+        let
+          createChromiumExtensionFor = browserVersion: { id, sha256, version }:
+            {
+              inherit id;
+              crxPath = builtins.fetchurl {
+                url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${browserVersion}&x=id%3D${id}%26installsource%3Dondemand%26uc";
+                name = "${id}.crx";
+                inherit sha256;
+              };
+              inherit version;
+            };
+          createChromiumExtension = createChromiumExtensionFor (lib.versions.major pkgs.ungoogled-chromium.version);
+        in
+        [
+          (createChromiumExtension {
+            # ublock origin
+            id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+            sha256 = "sha256:168vr0p31sp5ffsqnrnarw6ab1m95yil4hph0xs6gjbfky7wygki";
+            version = "1.65.0";
+          })
+          (createChromiumExtension {
+            # Proton pass
+            id = "ghmbeldphafepmbegfdlkpapadhbakde";
+            sha256 = "sha256:0582gc1k1xvhafmk60r0pgfxyiajq2rsk374hd2f6yd8jmaxb1zw";
+            version = "1.32.2";
+          })
+          (createChromiumExtension {
+            # Tabliss
+            id = "hipekcciheckooncpjeljhnekcoolahp";
+            sha256 = "sha256:05nd45vpyrrccilblrnmxh2bfnrn445g4f24z1qm8ncxdracgbfx";
+            version = "2.6.0";
+          })
+          (createChromiumExtension {
+            # Gruvbox-slate theme
+            id = "giokfhncgfjkoamdbhfhfhgpikaioccc";
+            sha256 = "sha256:0nv3213dd9s1mfxl6b4rhv6j11rdcl16zjklkj5g06b62imm7d1y";
+            version = "1.0";
+          })
+        ];
     };
   };
 
