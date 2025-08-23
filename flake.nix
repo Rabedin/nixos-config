61518240a2a2a30f67ddb0e92809c59547274d15
Redwanul Abedin <chromeplated@protonmail.com>
2025-08-22 22:07:30 -0400

    added pkgsUnstable to extraspecialargs

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
     homeConfigurations = {
       "red@nixbox" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
+        extraSpecialArgs = {inherit inputs pkgsUnstable outputs;};
         # > Our main home-manager configuration file <
         modules = [./home-manager/home.nix];
       };
