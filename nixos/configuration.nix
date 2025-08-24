b553de28b2b75082288f1326e6d4f93171953eb8
Redwanul Abedin <chromeplated@protonmail.com>
2025-08-24 15:24:54 -0400

    new IP schema

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -144,7 +144,7 @@
       fsType = "ext4";
     };
     "/mnt/redhomeshare" = {
-      device = "//192.168.0.72/redhomeshare";
+      device = "//192.168.69.102/redhomeshare";
       fsType = "cifs";
       options = let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
